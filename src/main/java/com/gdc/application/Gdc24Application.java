b03e9a1908b59d99c838fe1a59caed1c7802852c
odie100 <andriampeno.odilon@gmail.com>
2022-07-05 16:29:17 +0300

    project initialization

---
+++ src/main/java/com/gdc/application/Gdc24Application.java
@@ -0,0 +1,13 @@
+package com.gdc.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gdc24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gdc24Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdc/application/Gdc24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdc.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gdc24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
