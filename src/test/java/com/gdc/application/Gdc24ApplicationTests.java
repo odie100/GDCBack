cbfa5f483d4ae7d572c897195ced7c3c8536a577
dadanlah <78381149+dadanlah@users.noreply.github.com>
2022-07-06 13:54:34 +0300

    Update Gdc24ApplicationTests.java
    This is the cleaver Way to do this stuff

--- src/test/java/com/gdc/application/Gdc24ApplicationTests.java
+++ src/test/java/com/gdc/application/Gdc24ApplicationTests.java
@@ -2,6 +2,7 @@ package com.gdc.application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+// INA ZANY NENLAH TYHHH
 
 @SpringBootTest
 class Gdc24ApplicationTests {
